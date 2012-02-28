9fe98f9dbca58ceda0ea032f282faf442661e24c
demenev <demenev@gmail.com>
2012-02-28 07:58:26 +0000

    call ESerialPortManager::release() when releasing a port

--- src/eserialportproxy.cpp
+++ src/eserialportproxy.cpp
@@ -24,6 +24,7 @@ void ESerialPortProxy::grab()
 void ESerialPortProxy::release()
 {
     if (!isOwned) return;
+    ESerialPortManager::release(this);
 }
 
 bool ESerialPortProxy::open(QIODevice::OpenMode mode)
